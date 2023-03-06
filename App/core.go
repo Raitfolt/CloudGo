caadc443ff324ee240a93f704a58456eec5842f2
Антон Новосельцев <raitfolt@gmail.com>
2023-03-06 16:29:07 +0500

    153

--- App/core.go
+++ App/core.go
@@ -1,18 +1,148 @@
 package main
 
-import "errors"
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"sync"
+)
 
-var store = make(map[string]string)
+type TransactionLogger interface {
+	WriteDelete(key string)
+	WritePut(key, value string)
+	Err() <-chan error
+
+	ReadEvents() (<-chan Event, <-chan error)
+
+	Run()
+}
+
+func NewFileTransactionLogger(filename string) (TransactionLogger, error) {
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0755)
+	if err != nil {
+		return nil, fmt.Errorf("cannot open transaction log file: %w", err)
+	}
+
+	return &FileTransactionLogger{file: file}, nil
+}
+
+func (l *FileTransactionLogger) Run() {
+	events := make(chan Event, 16)
+	l.events = events
+
+	errors := make(chan error, 1)
+	l.errors = errors
+
+	go func() {
+		for e := range events {
+			l.lastSequence++
+
+			_, err := fmt.Fprintf(
+				l.file,
+				"%d\t%d\t%s\t%s\n",
+				l.lastSequence, e.EventType, e.Key, e.Value)
+			if err != nil {
+				errors <- err
+				return
+			}
+		}
+	}()
+}
+
+func (l *FileTransactionLogger) ReadEvents() (<-chan Event, <-chan error) {
+	scanner := bufio.NewScanner(l.file)
+	outEvent := make(chan Event)
+	outError := make(chan error, 1)
+
+	go func() {
+		var e Event
+
+		defer close(outEvent)
+		defer close(outError)
+
+		for scanner.Scan() {
+			line := scanner.Text()
+
+			if _, err := fmt.Sscanf(line, "%d\t%d\t%s\t%s",
+				&e.Sequence, &e.EventType, &e.Key, &e.Value); err != nil {
+				outError <- fmt.Errorf("input parse error: %w", err)
+				return
+			}
+
+			if l.lastSequence >= e.Sequence {
+				outError <- fmt.Errorf("transoction numbers out of sequence")
+
+				return
+			}
+
+			l.lastSequence = e.Sequence
+
+			outEvent <- e
+		}
+
+		if err := scanner.Err(); err != nil {
+			outError <- fmt.Errorf("transaction log read failure: %w", err)
+		}
+
+	}()
+
+	return outEvent, outError
+}
+
+type FileTransactionLogger struct {
+	events       chan<- Event
+	errors       <-chan error
+	lastSequence uint64
+	file         *os.File
+}
+
+func (l *FileTransactionLogger) WritePut(key, value string) {
+	l.events <- Event{EventType: EventPut, Key: key, Value: value}
+}
+
+func (l *FileTransactionLogger) WriteDelete(key string) {
+	l.events <- Event{EventType: EventDelete, Key: key}
+}
+
+func (l *FileTransactionLogger) Err() <-chan error {
+	return l.errors
+}
+
+type Event struct {
+	Sequence  uint64
+	EventType EventType
+	Key       string
+	Value     string
+}
+
+type EventType byte
+
+const (
+	_           = iota
+	EventDelete = iota
+	EventPut
+)
+
+var store = struct {
+	sync.RWMutex
+	m map[string]string
+}{m: make(map[string]string)}
 
 func Put(key string, value string) error {
-	store[key] = value
+	store.Lock()
+	store.m[key] = value
+	store.RUnlock()
+
 	return nil
 }
 
 var ErrorNoSuchKey = errors.New("no such key")
 
 func Get(key string) (string, error) {
-	value, ok := store[key]
+	store.RLock()
+	value, ok := store.m[key]
+	store.RUnlock()
 
 	if !ok {
 		return "", ErrorNoSuchKey
@@ -21,7 +151,9 @@ func Get(key string) (string, error) {
 }
 
 func Delete(key string) error {
-	delete(store, key)
+	store.Lock()
+	delete(store.m, key)
+	store.Unlock()
 
 	return nil
 }
